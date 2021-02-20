1366624ae538bcd07e263d55c27648858fc520e1
WKYong <ywk918@gmail.com>
2021-02-20 14:55:48 +0800

    update

--- model/generator.py
+++ model/generator.py
@@ -9,21 +9,28 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.downsample = nn.Sequential(ConvINLRelu(3, 64, 3, 1, 2, 1, bias=bias),
                                         ConvINLRelu(64, 128, 3, 1, 2, 1, bias),
-                                        ConvINLRelu(128, 256, 3, 1, 2, 1, bias),
-                                        ConvINLRelu(256, 512, 3, 1, 2, 1, bias))
-
+                                        ConvINLRelu(128, 256, 3, 1, 2, 1, bias),)
+        # ConvINLRelu(256, 512, 3, 1, 2, 1, bias))
+        """
         self.residual = nn.Sequential(InvertedResidual(512, 512, 1, 1, 2),
                                       InvertedResidual(512, 512, 1, 1, 2),
                                       InvertedResidual(512, 512, 1, 1, 2),
                                       InvertedResidual(512, 512, 1, 1, 2),
                                       InvertedResidual(512, 512, 1, 1, 2),
                                       InvertedResidual(512, 512, 1, 1, 2))
+        """
+        self.residual = nn.Sequential(InvertedResidual(256, 256, 1, 1, 2),
+                                      InvertedResidual(256, 256, 1, 1, 2),
+                                      InvertedResidual(256, 256, 1, 1, 2),
+                                      InvertedResidual(256, 256, 1, 1, 2),
+                                      InvertedResidual(256, 256, 1, 1, 2),
+                                      InvertedResidual(256, 256, 1, 1, 2))
 
-        self.upsample = nn.Sequential(Upsample(512),  # 512->256
-                                      Upsample(256),  # 256 -> 128
-                                      Upsample(128),  # 128 -> 64
-                                      Upsample(64),  # 64 -> 32
-                                      nn.Conv2d(32, 3, 3, 1, 1, bias=bias))
+        self.upsample = nn.Sequential(  # Upsample(512),  # 512->256
+            Upsample(256),  # 256 -> 128
+            Upsample(128),  # 128 -> 64
+            Upsample(64),  # 64 -> 32
+            nn.Conv2d(32, 3, 3, 1, 1, bias=bias))
 
         self.net = nn.Sequential(self.downsample,
                                  self.residual,
