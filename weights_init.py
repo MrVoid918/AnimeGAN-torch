30bce4ff3180510ff88fe11b85f3e77cb246591c
WKYong <ywk918@gmail.com>
2021-03-07 11:47:29 +0800

    update

--- weights_init.py
+++ weights_init.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 
 def weights_init(m):
     if isinstance(m, nn.Conv2d):
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        #nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.xavier_normal_(m.weight.data)
 
     if isinstance(m, nn.BatchNorm2d):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
